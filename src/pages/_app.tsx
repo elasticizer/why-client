51a4d169dad0dda4bd73bc684765405dc512f744
Felix <git@studio.elasticizer.dev>
2024-05-13 13:58:40 +0800

    Set up Preline UI & Tailwind CSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const pathname = usePathname();
+
+	useEffect(
+		() => void import('preline/preline')
+			.then(module => module.HSStaticMethods.autoInit()),
+		[pathname]
+	);
+
 	return <Component {...pageProps} />;
 }
