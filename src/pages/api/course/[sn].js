8e03f17fbe7070ef7744b80ec8af13ff1cf653af
Poyuan <poyuan510214@gmail.com>
2024-05-16 09:26:04 +0800

    course api

---
+++ src/pages/api/course/[sn].js
@@ -0,0 +1,8 @@
+import sequelize from '@/handlers/sequelize';
+
+export default async function handler(req, res) {
+	const sn = req.query.sn;
+	const data = await sequelize.models.Course.findByPk(sn);
+
+	return res.status(200).json(data);
+}
