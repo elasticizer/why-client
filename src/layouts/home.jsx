eca243bb6add98d356c2aff2ef5daa86f33f6a23
Poyuan <poyuan510214@gmail.com>
2024-05-16 10:08:02 +0800

    新增Layout

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,15 +1,12 @@
 import Navbar from '@/components/home/navbar.jsx';
 import Footer from '@/components/home/footer.jsx';
-import { CartProvider } from '../contexts/cart';
 
 export default function Home({ children }) {
-    return (
-        <>
-            <CartProvider>
-                <Navbar />
-                {children}
-                <Footer />
-            </CartProvider>
-        </>
-    );
+	return (
+		<>
+			<Navbar />
+			{children}
+			<Footer />
+		</>
+	);
 }
