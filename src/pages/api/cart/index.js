2f708e0b040347db7f22778bd610b7b61c316793
Peiru <lulu802790@gmail.com>
2024-05-17 17:17:34 +0800

    新增API與Navbar,Footer

--- src/pages/api/cart/index.js
+++ src/pages/api/cart/index.js
@@ -1,18 +1,9 @@
-import sequelize from '@/handlers/sequelize';
+import connection from '@/handlers/sqlite3';
 
 export default async function handler(req, res) {
 	const sn = req.query.sn;
-	const data = await sequelize.models.Course.findAll({
-		limit: 2
-		// order: sequelize.random(),
-		// include: [
-		// 	{
-		// 		model: Domain
-		// 	}
-		// ]
-	});
-	// 	const data = await sequelize.models.Course.findOne({
-	// 		order: sequelize.random()
-	// 	});
-	res.status(200).json(data);
+
+	const [results] = await connection.execute('SELECT * FROM Course');
+
+	res.status(200).json(results[0]);
 }
