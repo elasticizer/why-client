6bf81f58dac30582a2f53e2dc2dc2ebf188f356e
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-20 12:13:30 +0800

    新增購物車加入功能

--- src/components/cart/course/index.jsx
+++ src/components/cart/course/index.jsx
@@ -1,34 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import CourseCard from './courseCard';
 
-export default function CourseList() {
-	const [course, setCourse] = useState([]);
+export default function CourseList({ addItem = () => {} }) {
+	const [courses, setCourse] = useState([]);
 
 	useEffect(() => {
+		const getCourse = async () => {
+			const url = '/api/course';
+			try {
+				const res = await fetch(url);
+				const data = await res.json();
+				setCourse(data);
+			} catch (e) {
+				setCourse([]);
+			}
+		};
 		getCourse();
 	}, []);
 
-	const getCourse = async () => {
-		const url = '/api/course';
-		try {
-			const res = await fetch(url);
-			const data = await res.json();
-				setCourse(data);
-		} catch (e) {
-			setCourse([]);
-		}
-	};
-
 	return (
-		<>
-			{course.map((v, i) => (
-				<CourseCard
-					key={i}
-					name={v.Name}
-					price={v.Price}
-					intro={v.Intro}
-				/>
+		<div className="grid grid-cols-3 gap-4 my-4">
+			{courses.map((course, i) => (
+				<div
+					className="flex flex-col bg-white border-2 shadow-sm rounded-xl"
+					key={i}>
+					<div className="p-4 md:p-5 flex flex-col gap-3">
+						<h3 className="text-lg font-bold text-gray-800 dark:text-white">
+							{course.Name}
+						</h3>
+						<p>價格：{course.Price}</p>
+						<button
+							className="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+							onClick={() => addItem(course)}>
+							加到購物車
+						</button>
+					</div>
+				</div>
 			))}
-		</>
+		</div>
 	);
 }
