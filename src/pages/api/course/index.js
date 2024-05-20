5c924c9e22c6d2b591e92ad56019ab3a1461b8c7
Poyuan <poyuan510214@gmail.com>
2024-05-20 15:28:09 +0800

    更新資料庫串接

--- src/pages/api/course/index.js
+++ src/pages/api/course/index.js
@@ -1,11 +1,9 @@
-import sequelize from '@/handlers/sequelize';
+import connection from '@/handlers/sqlite3';
 
 export default async function handler(req, res) {
 	const limit = req.query.limit ?? 10;
 
-	const data = await sequelize.models.Course.findAll({
-		limit: 9
-	});
+	const [results] = await connection.execute('SELECT * FROM Course limit 15');
 
-	return res.status(200).json(data);
+	return res.status(200).json(results);
 }
