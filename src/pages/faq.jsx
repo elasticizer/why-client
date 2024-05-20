8876523f681ea3277f66812dd9820f8afcd5a7cb
Poyuan <poyuan510214@gmail.com>
2024-05-20 13:49:02 +0800

    新增常見問題內容&篩選功能

---
+++ src/pages/faq.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar2 from '@/components/head/navbar2';
+import Footer from '@/components/head/footer';
+import Faq from '../components/faq/faq';
+
+export default function FaqSection() {
+	return (
+		<>
+			<Navbar2 />
+			<Faq />
+			<Footer />
+		</>
+	);
+}
