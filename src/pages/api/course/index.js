968fdaf40f6a8496656131e6a625c4565344a478
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-21 00:52:24 +0800

    更新API、加入課程圖片

--- src/pages/api/course/index.js
+++ src/pages/api/course/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 	const limit = req.query.limit ?? 6;
 
 	const [results] = await connection.execute(
-		'SELECT Course.SN, Course.Identifier, Course.Name, Course.Price, Domain.Name as DomainName FROM Course LEFT JOIN Domain ON Course.DomainSN = Domain.SN ORDER BY RANDOM() LIMIT ?',
+		'SELECT Course.SN, Course.Identifier, Course.Name, Course.Price, Domain.Name AS DomainName, File.Filename FROM Course LEFT JOIN 	Domain ON Course.DomainSN = Domain.SN JOIN File ON Course.ThumbnailSN = File.SN ORDER BY RANDOM() LIMIT ?',
 		[limit]
 	);
 	res.status(200).json(results);
