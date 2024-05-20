eb5e3597c24675eccbac68f2236bc6142b44d034
Poyuan <poyuan510214@gmail.com>
2024-05-20 19:49:55 +0800

    串接資料庫 課程圖片

--- src/pages/api/course/index.js
+++ src/pages/api/course/index.js
@@ -3,7 +3,9 @@ import connection from '@/handlers/sqlite3';
 export default async function handler(req, res) {
 	const limit = req.query.limit ?? 10;
 
-	const [results] = await connection.execute('SELECT * FROM Course limit 15');
+	const [results] = await connection.execute(
+		'SELECT * FROM Course JOIN File ON Course.ThumbnailSN = File.SN '
+	);
 
 	return res.status(200).json(results);
 }
