a40f20704b324df131f7ed246e403985bdc3bdc8
Poyuan <poyuan510214@gmail.com>
2024-05-20 19:49:55 +0800

    串接資料庫 課程圖片

--- src/components/home/card-modules/ratingCard.jsx
+++ src/components/home/card-modules/ratingCard.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function RatingCard({ title, content }) {
+export default function RatingCard({ title, content, image }) {
 	return (
 		<>
 			<a
 				class="group block rounded-xl"
 				href="#">
-				<div class="aspect-w-16 aspect-h-9">
-					<Image
-						class="w-full object-cover rounded-xl"
-						width={100}
-						height={100}
-						src="/image/course.jpg"
-						alt={title}
-					/>
+				<img
+					class="w-full h-auto rounded-t-xl"
+					width={100}
+					height={100}
+					src={image}
+					alt="Image Description"
+				/>
+				<div class="p-4 md:p-5">
+					<h3 class="text-lg font-bold text-gray-800 dark:text-white">
+						{title}
+					</h3>
+					<p class="mt-1 text-gray-500 dark:text-neutral-400">{''}</p>
 				</div>
 				<h3 class="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-neutral-300 dark:group-hover:text-white">
 					{title}
