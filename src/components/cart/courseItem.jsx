e5950fa5ef08f140422abb1fe2af233225b03e48
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-21 14:24:37 +0800

    新增勾選狀態、修改其他元件&API

--- src/components/cart/courseItem.jsx
+++ src/components/cart/courseItem.jsx
@@ -43,9 +43,75 @@ export default function CourseItem({ item, checked, onChange, removeItem }) {
 						onClick={() => removeItem(item.SN)}>
 						<RiDeleteBinLine />
 					</button>
+					{/* 收藏Modal */}
+					<div
+						id="hs-basic-modal-collect"
+						className="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none">
+						<div className="sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+							<div className="flex flex-col bg-orange-50 border shadow-sm rounded-xl pointer-events-auto">
+								<div className="flex justify-between items-center py-3 px-4">
+									<h3 className="font-bold text-orange-800 text-sm">
+										已將「{item.Name}」移至收藏清單
+									</h3>
+									<button
+										type="button"
+										className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+										data-hs-overlay="#hs-basic-modal-collect">
+										<span className="sr-only">Close</span>
+										<svg
+											className="flex-shrink-0 size-4"
+											xmlns="http://www.w3.org/2000/svg"
+											width={24}
+											height={24}
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											strokeWidth={2}
+											strokeLinecap="round"
+											strokeLinejoin="round">
+											<path d="M18 6 6 18" />
+											<path d="m6 6 12 12" />
+										</svg>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* 刪除Modal */}
+					<div
+						id="hs-basic-modal-delete"
+						className="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none">
+						<div className="sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+							<div className="flex flex-col bg-red-50 border shadow-sm rounded-xl pointer-events-auto">
+								<div className="flex justify-between items-center py-3 px-4">
+									<h3 className="font-bold text-red-800 text-sm">
+										已將「{item.Name}」刪除
+									</h3>
+									<button
+										type="button"
+										className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
+										data-hs-overlay="#hs-basic-modal-delete">
+										<span className="sr-only">Close</span>
+										<svg
+											className="flex-shrink-0 size-4"
+											xmlns="http://www.w3.org/2000/svg"
+											width={24}
+											height={24}
+											viewBox="0 0 24 24"
+											fill="none"
+											stroke="currentColor"
+											strokeWidth={2}
+											strokeLinecap="round"
+											strokeLinejoin="round">
+											<path d="M18 6 6 18" />
+											<path d="m6 6 12 12" />
+										</svg>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-
-
 			</div>
 		</div>
 	);
