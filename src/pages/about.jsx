23597b115c09a838b88d2f78b25c1edccd46bd25
Poyuan <poyuan510214@gmail.com>
2024-05-21 11:15:09 +0800

    修改套件設定

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import Navbar from '@/components/head/navbar';
 import React from 'react';
-import Intro from '@/components/aboutUs/intro';
-import Faq from '../components/faq/faq';
-import Footer from '@/components/head/footer';
+import Intro from '@/components/home/aboutUs/intro';
+import Faq from '../components/home/faq/faq';
+import Faq2 from '../components/home/faq/faq2';
+import Home from '@/layouts/home';
+import Modal from '@/components/home/modal';
+import styles from '@/styles/modal.module.css';
 
 export default function About() {
+	const [showModal, setShowModal] = useState(false);
+
+	const toggleModal = () => {
+		setShowModal(!showModal);
+	};
 	return (
-		<>
-			<Navbar />
+		<Home>
+			<button
+				className={styles.floatingButton}
+				onClick={toggleModal}>
+				<Image
+					className={styles.floatingIcon}
+					src="/image/why_icon.png"
+					width={50}
+					height={50}
+					alt="按鈕圖片"
+				/>
+			</button>
+			<Modal show={showModal} />
 			<Intro />
 			<Faq />
 			<Footer />
