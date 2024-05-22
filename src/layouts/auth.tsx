9aebb5da320476812fb6fe4ce33dfcfd48f0aa17
Felix <git@studio.elasticizer.dev>
2024-05-22 21:24:45 +0800

    Make layout for authentication

---
+++ src/layouts/auth.tsx
@@ -0,0 +1,9 @@
+import { Progenitive } from '@/types';
+
+export default function AuthLayout({ children }: Progenitive) {
+	return (
+		<div className="bg-gray-100 flex h-screen items-center py-16 dark:bg-neutral-800">
+			<main className="w-full max-w-md mx-auto p-4">{children}</main>
+		</div>
+	);
+}
