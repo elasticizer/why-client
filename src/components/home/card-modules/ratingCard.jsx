1d52b212c350709e74ae9667943b423ec00c2677
Poyuan <poyuan510214@gmail.com>
2024-05-22 16:08:57 +0800

    class= -> className=

--- src/components/home/card-modules/ratingCard.jsx
+++ src/components/home/card-modules/ratingCard.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import Image from 'next/image';
 import Tags from '@/components/home/card-modules/tags';
 import { useEffect, useState } from 'react';
+import { FaFire } from 'react-icons/fa6';
 
 export default function RatingCard({ data }) {
 	return (
 		<a
-			data-aos="zoom-in-down"
-			class="flex flex-1 flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg transition"
+			className="flex flex-1 flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg transition overflow-hidden"
 			href="#">
 			<img
-				class="w-full h-auto rounded-t-xl"
-				width={100}
-				height={100}
+				className="w-full h-auto object-cover hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
 				src={data.Filename}
 				alt="Image Description"
 			/>
-			<div class="px-4 py-2 md:px-5">
-				<h3 class="text-lg font-bold text-gray-800 line-clamp-1">
+			<div className="px-4 py-2 md:px-5">
+				<h3 className="text-lg font-bold text-gray-800 line-clamp-1">
 					{data.Name}
 				</h3>
-				<p class="mt-1 text-gray-500 line-clamp-2">{data.Intro}</p>
+				<p className="mt-1 text-gray-500 line-clamp-2">{data.Intro}</p>
 			</div>
-			<div className="flex p-2">
+			<div className="flex p-2 items-center gap-2">
+				<div className="flex items-center gap-1">
+					<FaFire />
+					熱門課程
+				</div>
 				<Tags>{data.DomainName}</Tags>
 			</div>
 		</a>
