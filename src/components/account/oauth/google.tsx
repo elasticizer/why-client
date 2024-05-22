8e943832a974bf90c43a935246114d8253a86f2b
Felix <git@studio.elasticizer.dev>
2024-05-23 03:50:44 +0800

    Complete OAuth with Google

---
+++ src/components/account/oauth/google.tsx
@@ -0,0 +1,10 @@
+import { FcGoogle } from 'react-icons/fc';
+import OAuth from '.';
+
+export default function Google() {
+	return (
+		<OAuth href="" name="Google">
+			<FcGoogle size={24} />
+		</OAuth>
+	);
+}
---
+++ src/pages/api/auth/google/index.ts
@@ -0,0 +1,39 @@
+import { onError, onNoMatch } from '@/handlers/router';
+import connection from '@/handlers/sqlite3';
+import { serialize } from 'cookie';
+import { randomUUID } from 'crypto';
+import { StatusCodes } from 'http-status-codes';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
+import { env } from 'process';
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.get(async (_, res) => {
+	const uuid = randomUUID();
+	const target = 'https://accounts.google.com/o/oauth2/v2/auth';
+	const search = new URLSearchParams({
+		client_id: env.GOOGLE_CLIENT_ID as string,
+		prompt: 'select_account',
+		redirect_uri: env.GOOGLE_REDIRECT_URI as string,
+		response_type: 'code',
+		scope: [
+			'https://www.googleapis.com/auth/userinfo.email',
+			'https://www.googleapis.com/auth/userinfo.profile'
+		].join(' '),
+		state: uuid
+	});
+
+	const cookie = serialize('OAUTH_CSRF_TOKEN', uuid, {
+		maxAge: 3600,
+		httpOnly: true,
+		path: '/',
+		sameSite: 'lax',
+		secure: true
+	});
+
+	res.setHeader('Set-Cookie', cookie);
+	res.redirect(StatusCodes.SEE_OTHER, `${target}?${search.toString()}`);
+});
+
+export default router.handler({ onError, onNoMatch });
