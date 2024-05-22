9599b2a9ff4b5fcd797e55eb81170f672ece7ded
Felix <git@studio.elasticizer.dev>
2024-05-23 03:51:27 +0800

    Complete signout

---
+++ src/pages/signout/index.tsx
@@ -0,0 +1,36 @@
+import SessionLayout from '@/layouts/session';
+import type { ApiResponse } from '@/types';
+import { sleep } from '@/utilities';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function Index() {
+	const router = useRouter();
+	const [done, setDone] = useState(false);
+
+	useEffect(
+		() => void fetch('/api/auth/signout', { method: 'POST' })
+			.then(r => sleep(1000n, r))
+			.then(r => r.json())
+			.then(({ done }: ApiResponse<{}>) => setDone(done))
+			.then(() => sleep(1000n))
+			.then(() => router.replace('/')),
+		[]
+	);
+
+	return (
+		<SessionLayout>
+			<div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+				<div className="p-4 md:p-10">
+					<h3 className="text-lg font-bold text-gray-800 dark:text-white">
+						{done ? '您已經登出' : '您正在登出'}
+					</h3>
+
+					<p className="mt-2 text-gray-500 dark:text-neutral-400">
+						{done ? '正在將您導向至首頁。' : '請稍後。'}
+					</p>
+				</div>
+			</div>
+		</SessionLayout>
+	);
+}
