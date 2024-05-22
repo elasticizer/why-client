fe6c9e4015b8cd07016648a175e26c13a19301c3
Poyuan <poyuan510214@gmail.com>
2024-05-22 16:08:57 +0800

    class= -> className=

--- src/components/home/faq/faqCard.jsx
+++ src/components/home/faq/faqCard.jsx
@@ -6,27 +6,27 @@ export default function FaqCard({ question, children }) {
 	return (
 		<>
 			<div
-				class="hs-accordion pb-3 active"
+				className="hs-accordion pb-3 active"
 				id="hs-basic-with-title-and-arrow-stretched-heading-one">
 				<button
-					class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400"
+					className="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400"
 					aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
 					{question}
-					<IoIosArrowDown class="hs-accordion-active:hidden block flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
-					<IoIosArrowUp class="hs-accordion-active:block hidden flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
+					<IoIosArrowDown className="hs-accordion-active:hidden block flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
+					<IoIosArrowUp className="hs-accordion-active:block hidden flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
 				</button>
 				<div
 					id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-					class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+					className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
 					aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
-					<div class="text-gray-600 dark:text-neutral-400">{children}</div>
+					<div className="text-gray-600 dark:text-neutral-400">{children}</div>
 				</div>
 			</div>
 			{/* <div
-				class="hs-accordion pb-3 active"
+				className="hs-accordion pb-3 active"
 				id="hs-basic-with-title-and-arrow-stretched-heading-one">
 				<button
-					class="s-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500"
+					className="s-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500"
 					aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
 					{question}
 					<IoIosArrowDown className="hs-accordion-active:hidden block flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500" />
@@ -34,9 +34,9 @@ export default function FaqCard({ question, children }) {
 				</button>
 				<div
 					id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-					class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
+					className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
 					aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
-					<div class="text-gray-600 dark:text-neutral-400">{children}</div>
+					<div className="text-gray-600 dark:text-neutral-400">{children}</div>
 				</div>
 			</div> */}
 		</>
