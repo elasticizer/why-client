b573a1688b6f33c82aa534e4edbbbe9855687f58
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-22 11:45:52 +0800

    修正useCart

--- src/components/cart/cartList.jsx
+++ src/components/cart/cartList.jsx
@@ -9,7 +9,22 @@ export default function CartList() {
 	const { cartItem, removeItem } = useCart();
 
 	const [selectedItems, setSelectedItems] = useState([]);
+
 	const [course, setCourse] = useState([]);
+
+	useEffect(() => {
+		const getCourse = async () => {
+			const url = '/api/cart';
+			try {
+				const res = await fetch(url);
+				const data = await res.json();
+				setCourse(data);
+			} catch (e) {
+				setCourse([]);
+			}
+		};
+		getCourse();
+	}, []);
 
 	useEffect(() => {
 		const initState = cartItem.map((v, i) => {
@@ -18,27 +33,11 @@ export default function CartList() {
 		setCourse(initState);
 	}, [cartItem]);
 
-	const handleToggleChecked = id => {
-		const nextItems = course.map(v => {
-			if (v.id === id) return { ...v, checked: !v.checked };
-			else return v;
-		});
-		setCourse(nextItems);
+	const handleToggleChecked = sn => {
+		selectedItems.includes(sn)
+			? setSelectedItems(selectedItems.filter(v => v !== sn))
+			: setSelectedItems([...selectedItems, sn]);
 	};
-
-	const handleToggleCheckAll = e => {
-		const nextItems = course.map(v => {
-			return { ...v, checked: e.target.checked };
-		});
-		setCourse(nextItems);
-	};
-
-	useEffect(() => {
-		const selected = course.filter(v => v.checked);
-		setSelectedItems(selected);
-		console.log(selected);
-	}, [course]);
-	// 
 
 	return (
 		<div className="border border-gray-300 rounded-lg">
@@ -47,18 +46,19 @@ export default function CartList() {
 			) : (
 				<>
 					<CheckAll
-						checked={course.length > 0 && course.every(v => v.checked)}
-						onChange={handleToggleCheckAll}
+						checked={selectedItems.length === course.length}
+						onChange={() =>
+							selectedItems.length === course.length
+								? setSelectedItems([])
+								: setSelectedItems(course.map(v => v.SN))
+						} // TODO id改為SN就行了
 					/>
 					{course.map(item => (
 						<CourseItem
-							key={item.id}
+							key={item.SN}
 							item={item}
-							checked={
-								course.find(v => v.id === item.id) &&
-								course.find(v => v.id === item.id).checked
-							}
-							onChange={() => handleToggleChecked(item.id)}
+							checked={selectedItems.includes(item.SN)}
+							onChange={() => handleToggleChecked(item.SN)}
 							removeItem={removeItem}
 						/>
 					))}
