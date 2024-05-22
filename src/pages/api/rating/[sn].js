5d23bee75ab872dc5d3b674958e6349720227de6
Poyuan <poyuan510214@gmail.com>
2024-05-22 16:23:57 +0800

    新增熱門課程api

---
+++ src/pages/api/rating/[sn].js
@@ -0,0 +1,11 @@
+import connection from '@/handlers/sqlite3';
+
+export default async function handler(req, res) {
+	const sn = req.query.sn;
+	const [results] = await connection.execute(
+		'SELECT * FROM Course WHERE SN=?',
+		[sn]
+	);
+
+	return res.status(200).json(results[0]);
+}
