4ab0bcb959383a5ac7818e10ed4001d7ed3c1793
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-22 16:06:50 +0800

    完成購物車新增、刪除API

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -5,19 +5,46 @@ const CartContext = createContext(null);
 export function CartProvider({ children }) {
 	const [cartItem, setCartItem] = useState([]);
 
-	const addItemToCart = course => {
+	const addItemToCart = async course => {
 		if (!cartItem.some(item => item.SN === course.SN)) {
+			const fd = new FormData();
+
+			fd.append('user', 1);
+			fd.append('course', course.SN);
+
+			const data = await fetch('/api/cart', {
+				method: 'POST',
+				body: new URLSearchParams(fd)
+			}).then(r => r.json());
+
+			if (!data.done) {
+				return;
+			}
+
 			const newItem = { ...course, qty: 1 };
 			const nextItem = [newItem, ...cartItem];
+
 			setCartItem(nextItem);
 		} else {
 			alert(`${course.Name} 已經在購物車`);
 		}
 	};
 
-	const removeItem = id => {
-		const nextItems = cartItem.filter(item => item.SN !== id);
-		setCartItem(nextItems);
+	const removeItem = async course => {
+		const fd = new FormData();
+
+		fd.append('user', 1);
+		fd.append('course', course.SN);
+
+		const data = await fetch('/api/cart', {
+			method: 'DELETE',
+			body: new URLSearchParams(fd)
+		});
+
+		if (data.done) {
+			const nextItems = cartItem.filter(item => item.SN !== course.SN);
+			setCartItem(nextItems);
+		}
 	};
 
 	const totalQty = cartItem.reduce((acc, v) => acc + v.qty, 0);
