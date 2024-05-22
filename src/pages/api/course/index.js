3f8c123216078b92e212001efc784fa516b684db
Poyuan <poyuan510214@gmail.com>
2024-05-22 09:30:12 +0800

    join資料庫table 課程、圖片檔、分類標籤

--- src/pages/api/course/index.js
+++ src/pages/api/course/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 	const limit = req.query.limit ?? 10;
 
 	const [results] = await connection.execute(
-		'SELECT * FROM Course JOIN File ON Course.ThumbnailSN = File.SN '
+		'SELECT Course.SN, Course.Name, Course.Intro,File.FileName, Domain.Name AS DomainName FROM Course JOIN File ON Course.ThumbnailSN = File.SN JOIN Domain ON Course.DomainSN = Domain.SN'
 	);
 
 	return res.status(200).json(results);
