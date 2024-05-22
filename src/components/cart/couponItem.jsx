276be39c0a1be3b91dbbbe8a8299e5cff3aafe0d
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-22 10:43:05 +0800

    新增優惠券假資料

--- src/components/cart/couponItem.jsx
+++ src/components/cart/couponItem.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
 export default function CouponItem() {
+	const couponData = [
+		{ id: 1, title: '隨機優惠', directions: '隨機優惠券，折扣10%' },
+		{ id: 2, title: '畢業生專屬', directions: '畢業生專屬優惠，折扣50%' },
+		{ id: 3, title: '端午節優惠', directions: '端午節特別優惠，享受額外30%折扣' }
+	];
+
 	return (
 		<>
-			<a className="max-w-xs flex flex-col border border-t-4 border-t-orange-400 hover:border-t-4 hover:border-t-orange-600 shadow-sm rounded-xl cursor-pointer">
-				<div className="p-4 md:p-5">
-					<h3 className="text-lg font-bold text-gray-800 dark:text-white">
-						全館享9折優惠
-					</h3>
-					<p className="mt-2 text-gray-500 dark:text-neutral-400">
-						1000元變成900元
-					</p>
-				</div>
-			</a>
+			{couponData.map((v,i) => (
+				<a
+					key={i}
+					className="max-w-xs flex flex-col border border-t-4 border-t-orange-400 hover:border-t-4 hover:border-t-orange-600 shadow-sm rounded-xl cursor-pointer w-1/4">
+					<div className="p-4 md:p-5">
+						<h3 className="text-lg text-center font-bold text-gray-800">
+							{v.title}
+						</h3>
+						<p className="mt-2 text-gray-500 text-sm text-center">
+							{v.directions}
+						</p>
+					</div>
+				</a>
+			))}
 		</>
 	);
 }
