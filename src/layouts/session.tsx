07d2f11dcc0d63316efe385f4de86a3bf0e24847
Felix <git@studio.elasticizer.dev>
2024-05-22 21:24:45 +0800

    Make layout for session

---
+++ src/layouts/session.tsx
@@ -0,0 +1,11 @@
+import { Progenitive } from '@/types/react';
+
+export default function SessionLayout({ children }: Progenitive) {
+	return (
+		<div className="bg-gray-100 flex h-screen items-center py-16 dark:bg-neutral-800">
+			<main className="w-full max-w-md mx-auto p-4">
+				{children}
+			</main>
+		</div>
+	);
+}
