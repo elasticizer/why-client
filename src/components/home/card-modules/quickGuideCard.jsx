94644e9f05bbfc737fd749255d339fd4c4da07ee
Poyuan <poyuan510214@gmail.com>
2024-05-22 16:08:57 +0800

    class= -> className=

--- src/components/home/card-modules/quickGuideCard.jsx
+++ src/components/home/card-modules/quickGuideCard.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function QuickGuideCard({ title, content }) {
 	return (
 		<>
-			<div class="flex flex-1 flex-col bg-transparent border border-t-4 border-t-orange-600 shadow-sm rounded-2xl hover:shadow-lg transition">
-				<div class=" p-4 md:p-5 hover:transition-transform duration-500 ease-in-out">
-					<h3 class="text-lg font-bold">{title}</h3>
-					<p class="mt-2">{content}</p>
+			<div className="flex flex-1 flex-col backdrop-blur border border-t-4 border-t-orange-600 shadow-sm rounded-2xl hover:shadow-lg hover:scale-105 transition">
+				<div className=" p-4 md:p-5 hover:transition-transform duration-500 ease-in-out">
+					<h3 className="text-lg font-bold">{title}</h3>
+					<p className="mt-2">{content}</p>
 					<a
-						class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-sky-500 hover:text-sky-600 disabled:opacity-50 disabled:pointer-events-none"
+						className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent hover:text-gray-600 disabled:opacity-50 disabled:pointer-events-none"
 						href="#">
 						立即前往
 						<svg
