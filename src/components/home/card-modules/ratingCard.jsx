0e111aeb4447c81b14ef45973d2f0369d020e711
Poyuan <poyuan510214@gmail.com>
2024-05-22 09:28:49 +0800

    調整熱門課程區域資料庫串接&加上標籤

--- src/components/home/card-modules/ratingCard.jsx
+++ src/components/home/card-modules/ratingCard.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
+import Tags from '@/components/home/card-modules/tags';
+import { useEffect, useState } from 'react';
 
-export default function RatingCard({ title, content, image }) {
+export default function RatingCard({ data }) {
 	return (
-		<>
-			<a
-				class="group block rounded-xl"
-				href="#">
-				<img
-					class="w-full h-auto rounded-t-xl"
-					width={100}
-					height={100}
-					src={image}
-					alt="Image Description"
-				/>
-				<div class="p-4 md:p-5">
-					<h3 class="text-lg font-bold text-gray-800 dark:text-white">
-						{title}
-					</h3>
-					<p class="mt-1 text-gray-500 dark:text-neutral-400">{''}</p>
-				</div>
-				<h3 class="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-neutral-300 dark:group-hover:text-white">
-					{title}
+		<a
+			data-aos="zoom-in-down"
+			class="flex flex-1 flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg transition"
+			href="#">
+			<img
+				class="w-full h-auto rounded-t-xl"
+				width={100}
+				height={100}
+				src={data.Filename}
+				alt="Image Description"
+			/>
+			<div class="px-4 py-2 md:px-5">
+				<h3 class="text-lg font-bold text-gray-800 line-clamp-1">
+					{data.Name}
 				</h3>
-				<p class="mt-2 text-sm text-gray-600 dark:text-neutral-400">
-					{content}
-				</p>
-			</a>
-		</>
+				<p class="mt-1 text-gray-500 line-clamp-2">{data.Intro}</p>
+			</div>
+			<div className="flex p-2">
+				<Tags>{data.DomainName}</Tags>
+			</div>
+		</a>
 	);
 }
