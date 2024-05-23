e2b5b0980084239ea1d5fac0161288f099338909
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-23 14:19:23 +0800

    修正購物車價錢問題

--- src/components/cart/courseItem.jsx
+++ src/components/cart/courseItem.jsx
@@ -1,33 +1,33 @@
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { TbHeartPlus } from 'react-icons/tb';
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 export default function CourseItem({ item, checked, onChange, removeItem }) {
-	const MySwal = withReactContent(Swal)
+	const MySwal = withReactContent(Swal);
 
-  const notifyAndRemove = (item) => {
-    MySwal.fire({
-      title: '確定要刪除嗎？',
-      text: '你將無法回復這個操作！',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      cancelButtonText: '取消',
-      confirmButtonText: '確定刪除',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        MySwal.fire({
-          title: '已刪除',
-          text: item.Name + ' 已從購物車中刪除',
-          icon: 'success',
-        })
+	const notifyAndRemove = item => {
+		MySwal.fire({
+			title: '確定要刪除嗎？',
+			text: '你將無法回復這個操作！',
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			cancelButtonText: '取消',
+			confirmButtonText: '確定刪除'
+		}).then(result => {
+			if (result.isConfirmed) {
+				MySwal.fire({
+					title: '已刪除',
+					text: item.Name + ' 已從購物車中刪除',
+					icon: 'success'
+				});
 
-        removeItem(item)
-      }
-    })
-  }
+				removeItem(item);
+			}
+		});
+	};
 	return (
 		<div className="flex flex-row sm:flex-wrap md:flex-nowrap lg:flex-nowrap justify-between items-center p-4 gap-3 h-full">
 			<div className="flex items-start sm:gap-3">
@@ -64,7 +64,6 @@ export default function CourseItem({ item, checked, onChange, removeItem }) {
 					</button>
 					<button
 						className="text-red-400 hover:text-red-600"
-						data-hs-overlay="#hs-basic-modal-delete"
 						onClick={() => notifyAndRemove(item)}>
 						<RiDeleteBinLine /> {/* 刪除按鈕 */}
 					</button>
@@ -102,40 +101,6 @@ export default function CourseItem({ item, checked, onChange, removeItem }) {
 							</div>
 						</div>
 					</div>
-					{/* 刪除Modal */}
-					{/* <div
-						id="hs-basic-modal-delete"
-						className="hs-overlay hs-overlay-open:opacity-100 hs-overlay-open:duration-500 hidden size-full fixed top-0 start-0 z-[80] opacity-0 overflow-x-hidden transition-all overflow-y-auto pointer-events-none">
-						<div className="sm:max-w-lg sm:w-full m-3 sm:mx-auto">
-							<div className="flex flex-col bg-red-50 border shadow-sm rounded-xl pointer-events-auto">
-								<div className="flex justify-between items-center py-3 px-4">
-									<h3 className="font-bold text-red-800 text-sm">
-										已將「{item.Name}」刪除
-									</h3>
-									<button
-										type="button"
-										className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
-										data-hs-overlay="#hs-basic-modal-delete">
-										<span className="sr-only">Close</span>
-										<svg
-											className="flex-shrink-0 size-4"
-											xmlns="http://www.w3.org/2000/svg"
-											width={24}
-											height={24}
-											viewBox="0 0 24 24"
-											fill="none"
-											stroke="currentColor"
-											strokeWidth={2}
-											strokeLinecap="round"
-											strokeLinejoin="round">
-											<path d="M18 6 6 18" />
-											<path d="m6 6 12 12" />
-										</svg>
-									</button>
-								</div>
-							</div>
-						</div>
-					</div> */}
 				</div>
 			</div>
 		</div>
--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react';
 import CartList from '@/components/cart/cartList';
 import Summary from '@/components/cart/summary';
 import PrevPageBtn from './prevPageBtn';
-import CourseList from './course';
 import { useCart } from '@/hooks/useCart';
-import Link from 'next/link';
 
 export default function Cart() {
-	const { totalPrice, totalQty, cartItem, removeItem } = useCart();
+	const { cartItem, removeItem } = useCart();
 
 	return (
 		<div className="container mt-8 max-w-[85rem] sm:px-6 lg:px-8 mx-auto my-8">
@@ -15,7 +12,6 @@ export default function Cart() {
 				href="/test"
 				prev="課程頁"
 			/>
-			{/* <CourseList addItemToCart={addItemToCart} /> 測試的課程列表 */}
 			<div className="grid lg:grid-cols-4 gap-y-8 lg:gap-y-0 lg:gap-x-6">
 				<div className="lg:col-span-3">
 					<CartList
@@ -24,10 +20,7 @@ export default function Cart() {
 					/>
 				</div>
 				<div className="lg:col-span-1 lg:w-full lg:h-fit lg:via-transparent lg:to-transparent dark:from-neutral-800 border border-gray-300 rounded-lg">
-					<Summary
-						totalQty={totalQty}
-						totalPrice={totalPrice}
-					/>
+					<Summary />
 				</div>
 			</div>
 		</div>
