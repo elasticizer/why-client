c66e7f0816910dd8345ab1167c4fd11d70cb7897
Poyuan <poyuan510214@gmail.com>
2024-05-23 13:49:23 +0800

    修正預設欄位不會隱藏問題

--- src/components/home/faq/faqCard.jsx
+++ src/components/home/faq/faqCard.jsx
@@ -1,44 +1,30 @@
-import React from 'react';
+import React, { useId } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { IoIosArrowUp } from 'react-icons/io';
 
 export default function FaqCard({ question, children }) {
+	const headingId = useId();
+	const contentId = useId();
+
 	return (
 		<>
 			<div
-				className="hs-accordion pb-3 active"
-				id="hs-basic-with-title-and-arrow-stretched-heading-one">
+				class="hs-accordion pt-6 pb-3"
+				id={headingId}>
 				<button
-					className="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400"
-					aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
+					class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400"
+					aria-controls={contentId}>
 					{question}
-					<IoIosArrowDown className="hs-accordion-active:hidden block flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
-					<IoIosArrowUp className="hs-accordion-active:block hidden flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
+					<IoIosArrowDown class="hs-accordion-active:hidden block flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
+					<IoIosArrowUp class="hs-accordion-active:block hidden flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400" />
 				</button>
 				<div
-					id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-					className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-					aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
-					<div className="text-gray-600 dark:text-neutral-400">{children}</div>
+					id={contentId}
+					class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+					aria-labelledby={headingId}>
+					<div class="text-gray-600 dark:text-neutral-400">{children}</div>
 				</div>
 			</div>
-			{/* <div
-				className="hs-accordion pb-3 active"
-				id="hs-basic-with-title-and-arrow-stretched-heading-one">
-				<button
-					className="s-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500"
-					aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one">
-					{question}
-					<IoIosArrowDown className="hs-accordion-active:hidden block flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500" />
-					<IoIosArrowUp className="hs-accordion-active:block hidden flex-shrink-0 size-5 text-gray-600 group-hover:text-gray-500" />
-				</button>
-				<div
-					id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-					className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-					aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one">
-					<div className="text-gray-600 dark:text-neutral-400">{children}</div>
-				</div>
-			</div> */}
 		</>
 	);
 }
