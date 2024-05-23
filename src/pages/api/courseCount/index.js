b5bf9de0523965e1b5a944085510bb13f916f95b
Poyuan <poyuan510214@gmail.com>
2024-05-23 17:11:34 +0800

    api更新

--- src/pages/api/courseCount/index.js
+++ src/pages/api/courseCount/index.js
@@ -2,7 +2,7 @@ import connection from '@/handlers/sqlite3';
 
 export default async function handler(req, res) {
 	const limit = req.query.limit ?? 10;
-	const [results] = await connection.execute(
+	const [[results]] = await connection.execute(
 		'SELECT COUNT(Course.Name) AS TotalCourses FROM Course JOIN User ON Course.TeacherSN = User.SN'
 	);
 
--- src/pages/api/income/index.js
+++ src/pages/api/income/index.js
@@ -1,10 +1,19 @@
 import connection from '@/handlers/sqlite3';
+import Session from '@/helpers/session';
+import { RouteError } from '@/handlers/router';
 
 export default async function handler(req, res) {
+	const sessionId = req.cookies.SESSION_ID;
+	if (!sessionId) {
+		throw new RouteError(StatusCodes.FORBIDDEN, '沒有登入');
+	}
+
+	const user = await Session.associate(sessionId);
+
 	const limit = req.query.limit ?? 10;
 	const [results] = await connection.execute(
-		'SELECT Course.*, User.SN AS UserSN, User."FirstName", User."LastName", User."Nickname" FROM Course JOIN User ON Course.TeacherSN = User.SN WHERE User.SN = 4 LIMIT ?',
-		[limit]
+		'SELECT Course.*, User.SN AS UserSN, User."FirstName", User."LastName", User."Nickname" FROM Course JOIN User ON Course.TeacherSN = User.SN WHERE User.SN = ? LIMIT ?',
+		[user.SN, limit]
 	);
 
 	return res.status(200).json(results);
