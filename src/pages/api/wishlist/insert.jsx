63cc6f5574da36c1dd26bb8f2bec075b5eced1f1
chun <>
2024-05-23 17:10:51 +0800

    0523 提交

--- src/pages/api/wishlist/insert.jsx
+++ src/pages/api/wishlist/insert.jsx
@@ -1,10 +1,22 @@
 import connection from '@/handlers/sqlite3';
 import { createRouter } from "next-connect";
+import Session from '@/helpers/session';
+import { RouteError } from '@/handlers/router';
 
 const router = createRouter();
 
 // 定義 GET 請求的處理程序
 router.get(async (req, res) => {
+    const sessionId = req.cookies.SESSION_ID;
+
+    if (!sessionId) {
+        throw new RouteError(
+            StatusCodes.FORBIDDEN,
+            '沒有登入'
+        );
+    }
+    const user = await Session.associate(sessionId);
+    
     try {
         const { userid, courseSN } = req.query
         const [results] = await connection.execute("INSERT INTO CollectedCourse VALUES (?,?)", [userid, courseSN])
