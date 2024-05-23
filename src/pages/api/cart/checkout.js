9b124d750a38ac9de56815f47917df9228020f8d
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-23 10:44:43 +0800

    API更新

--- src/pages/api/cart/checkout.js
+++ src/pages/api/cart/checkout.js
@@ -1,12 +1,41 @@
 import connection from '@/handlers/sqlite3';
+import { createRouter } from 'next-connect';
 
-export default async function handler(req, res) {
-	const limit = req.query.limit ?? 3;
-  const domainSN = req.query.domainSN ?? 24;
+const router = createRouter();
 
+router.get(async (req, res) => {
 	const [results] = await connection.execute(
-		'SELECT Course.SN, Course.Identifier, Course.Name, Course.Price, Domain.Name as DomainName FROM Course LEFT JOIN Domain ON Course.DomainSN = Domain.SN WHERE Course.DomainSN = ? LIMIT ?',
-		[domainSN, limit]
+		'SELECT Course.SN, Course.Name, Course.Price, Domain.Name AS DomainName, Cart.UserSN, Cart.CourseSN FROM Cart JOIN Course ON Course.SN = Cart.CourseSN JOIN User ON User.SN = Cart.UserSN JOIN Domain ON Course.DomainSN = Domain.SN'
 	);
 	res.status(200).json(results);
-}
+});
+
+router.post(async (req, res) => {
+	const { user, payment } = req.body;
+	if (!user || !course) {
+		return res.status(400).json({ done: false, message: '缺少必要欄位' });
+	}
+	const [results] = await connection.execute(
+		'INSERT INTO Cart (UserSN, CourseSN) VALUES (?, ?)',
+		[user, payment]
+	);
+	res.status(200).json({ done: true, message: '新增課程成功' });
+});
+
+router.delete(async (req, res) => {
+	const { user, course } = req.body;
+	if (!user || !course) {
+		return res.status(400).json({ done: false, message: '缺少必要欄位' });
+	}
+	const [results] = await connection.execute(
+		'DELETE FROM Cart WHERE UserSN = ? AND CourseSN = ?',
+		[user, course]
+	);
+	res.status(204).json({});
+});
+
+export default router.handler({
+	onError(e, req, res) {
+		res.status(500).json({ message: '已經有資料' });
+	}
+});
--- src/pages/api/transaction/index.js
+++ src/pages/api/transaction/index.js
@@ -5,18 +5,21 @@ const router = createRouter();
 
 router.get(async (req, res) => {
 	const [results] = await connection.execute(
-		'SELECT Course.SN, Course.Identifier, Course.Name, Course.Price, Domain.Name AS DomainName, Cart.CourseSN FROM Course LEFT JOIN	Domain ON Course.DomainSN = Domain.SN JOIN Cart ON Course.SN = Cart.CourseSN'
+		'SELECT "Order"."SN", "Order"."WhenCheckedOut", "Order"."WhenPaid", "OrderDetail"."Subtotal", Course.Name, Course.Price, File.Filename FROM "Order" JOIN "OrderDetail" ON "Order"."SN" = "OrderDetail"."OrderSN" JOIN Course ON Course.SN = OrderDetail.CourseSN JOIN File ON Course.ThumbnailSN = File.SN'
 	);
+	// 'SELECT "Order"."WhenCheckedOut", "Order"."WhenPaid", "OrderDetail"."Subtotal", Course.Name, Course.Price, Coupon.DiscountRate FROM "Order" JOIN "OrderDetail" ON "Order"."SN" = "OrderDetail"."OrderSN" JOIN Course ON Course.SN = OrderDetail.CourseSN JOIN Coupon ON Order.CouponSN = Coupon.CreatorSN'
 	res.status(200).json(results);
 });
+
 router.post(async (req, res) => {
-	const { learner } = req.body;
+	const { learner, coupon } = req.body;
 	const [results] = await connection.execute(
-		'INSERT INTO Order (UserSN, CourseSN) VALUES (?, ?)',
-		[learner]
+		'INSERT INTO "Order" (LearnerSN,CouponSN) VALUES (?,?)',
+		[learner, coupon]
 	);
-	res.status(200).json(results);
+	res.status(200).json({ done: true, message: '新增訂單成功' });
 });
+
 export default router.handler({
 	onError(e, req, res) {
 		res.status(500).json({ message: '已經有資料' });
