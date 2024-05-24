bdefaa2a41ce1094dc986f7d6401eb337328403f
Poyuan <poyuan510214@gmail.com>
2024-05-24 17:15:42 +0800

    修改api架構

--- src/pages/api/article/index.js
+++ src/pages/api/article/index.js
@@ -1,7 +1,12 @@
+import { onError, onNoMatch } from '@/handlers/router';
 import connection from '@/handlers/sqlite3';
+import { StatusCodes } from 'http-status-codes';
+import { createRouter } from 'next-connect';
 import { stripHtml } from 'string-strip-html';
 
-export default async function handler(req, res) {
+const router = createRouter();
+
+router.get(async (req, res) => {
 	const limit = req.query.limit ?? 10;
 	const [results] = await connection.execute(
 		'SELECT * FROM Article JOIN User ON Article.AuthorSN = User.SN LIMIT ?',
@@ -13,4 +18,6 @@ export default async function handler(req, res) {
 	}));
 
 	return res.status(200).json(data);
-}
+});
+
+export default router.handler({ onError, onNoMatch });
--- src/pages/api/courseCount/index.js
+++ src/pages/api/courseCount/index.js
@@ -1,10 +1,36 @@
+import { RouteError } from '@/handlers/router';
 import connection from '@/handlers/sqlite3';
+import Session from '@/helpers/session';
+import { createRouter } from 'next-connect';
+import { onError, onNoMatch } from '@/handlers/router';
+import { StatusCodes } from 'http-status-codes';
 
-export default async function handler(req, res) {
-	const limit = req.query.limit ?? 10;
-	const [[results]] = await connection.execute(
-		'SELECT COUNT(Course.Name) AS TotalCourses FROM Course JOIN User ON Course.TeacherSN = User.SN'
+const router = createRouter();
+
+router.get(async (req, res) => {
+	const sessionId = req.cookies.SESSION_ID;
+
+	if (!sessionId) {
+		throw new RouteError(StatusCodes.UNAUTHORIZED, '沒有登入');
+	}
+
+	const user = await Session.associate(sessionId);
+
+	const [data] = await connection.execute(
+		'SELECT Course.Name,Course.Identifier,Course.Price, Course.WhenLaunched,`Order`.SN AS OrderSN, `Order`.WhenPaid,SUM(Course.Price) AS Income, COUNT(*) AS Total FROM Course JOIN User ON User.SN = Course.TeacherSN JOIN OrderDetail ON OrderDetail.CourseSN = Course.SN JOIN `Order` ON `Order`.SN = OrderDetail.OrderSN WHERE Course.WhenLaunched IS NOT NULL AND `Order`.WhenPaid IS NOT NULL AND User.SN = ? GROUP BY Course.SN',
+		[user.SN]
 	);
+	console.log(data);
+	res.status(200).json(data);
+});
 
-	return res.status(200).json(results);
-}
+export default router.handler({ onError, onNoMatch });
+
+// export default async function handler(req, res) {
+// 	const limit = req.query.limit ?? 10;
+// 	const [[results]] = await connection.execute(
+// 		'SELECT COUNT(Course.Name) AS TotalCourses FROM Course JOIN User ON Course.TeacherSN = User.SN'
+// 	);
+
+// 	return res.status(200).json(results);
+// }
