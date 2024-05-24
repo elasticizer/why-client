edb37aa7b0e4cdcbc83d863ffb828831664103f0
Poyuan <poyuan510214@gmail.com>
2024-05-24 17:16:42 +0800

    登入登出連結修改 localhost:3000 -> /

--- src/components/home/loginSession.jsx
+++ src/components/home/loginSession.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 import { FiUser } from 'react-icons/fi';
 
 export default function LoginSession() {
 	return (
-		<a
+		<Link
 			className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-orange-400 md:border-s md:border-gray-300 md:my-6 md:ps-6"
-			href="http://localhost:3000/signin">
+			href="/signin">
 			<FiUser />
 			登入/註冊
-		</a>
+		</Link>
 	);
 }
