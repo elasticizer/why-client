26d1e1baf90f8ddb16f8bf19dc4a964ab6117f90
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-26 15:44:13 +0800

    刪除work頁面

--- src/pages/api/work/index.js
+++
@@ -1,9 +0,0 @@
-import connection from '@/handlers/sqlite3';
-
-
-export default async function handler(req, res) {
-	const sn = req.query.sn;
-	const [results] = await connection.execute('SELECT * FROM User');
-
-	res.status(200).json(results[0]);
-}
