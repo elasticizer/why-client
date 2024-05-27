015871d9ab0d71ca6d059904e12969158271f5f1
Poyuan <poyuan510214@gmail.com>
2024-05-27 14:09:46 +0800

    調整講師收益頁面&路徑

--- src/pages/income/index.jsx
+++ src/pages/income/index.jsx
@@ -1,13 +1,36 @@
-import MainLayout from '@/layouts/mainLayout';
+import Header from '@/components/member/teacher/header';
+import Sidebar from '@/components/member/teacher/sidebar';
+import Tabbar from '@/components/member/tabbarTeacher';
+import UploadCard from '@/components/member/teacher/uploadCard';
+import Content from '@/components/member/teacher/content';
+import Search from '@/components/member/search';
+import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import React from 'react';
+import styles from '@/styles/teacher.module.css';
+import Link from 'next/link';
 import IncomePage from '@/components/income';
 
-export default function InCome() {
+export default function Index() {
+	const [windowNav, setWindowNav] = useState('hidden');
 	return (
 		<>
-			<MainLayout>
+			<div className={`${styles.container}`}>
+				{/* header */}
+				<Header />
 				<title>講師收益</title>
 				<IncomePage />
-			</MainLayout>
+			</div>
+			<Tabbar
+				setWindowNav={setWindowNav}
+				windowNav={windowNav}
+			/>
+
+			{/* SECTION mobileWindowNavbar */}
+			<Sidebar
+				WindowNav={windowNav}
+				setWindowNav={setWindowNav}
+			/>
 		</>
 	);
 }
