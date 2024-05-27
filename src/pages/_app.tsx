d9d81f0a18c77a3bcf0b3a99abad428cb4a03898
Felix <git@studio.elasticizer.dev>
2024-05-28 03:40:41 +0800

    https://gist.github.com/nikparo/33544fe0228dd5aa6f0de8d03e96c378

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,32 @@ import '@/styles/globals.css';
 import { SessionProvider } from '@/contexts/session';
 import type { AppProps } from 'next/app';
 import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { useCallback, useEffect, type EffectCallback } from 'react';
+
+type EffectProps = {
+	callback: EffectCallback;
+};
+
+const Effect = ({ callback }: EffectProps) => (
+	console.log(Effect.name),
+	useEffect(callback, [callback]),
+	null
+);
 
 export default function App({ Component, pageProps }: AppProps) {
 	const pathname = usePathname();
-
-	useEffect(
+	const callback = useCallback(
 		() => void import('preline/preline')
 			.then(module => module.HSStaticMethods.autoInit()),
 		[pathname]
 	);
 
 	return (
-		<SessionProvider>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<>
+			<SessionProvider>
+				<Component {...pageProps} />
+			</SessionProvider>
+			<Effect {...{ callback }} />
+		</>
 	);
 }
