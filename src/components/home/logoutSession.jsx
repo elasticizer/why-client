96650ecc34432a76e68decf401d2575454409aea
Poyuan <poyuan510214@gmail.com>
2024-05-27 14:09:46 +0800

    調整講師收益頁面&路徑

--- src/components/home/logoutSession.jsx
+++ src/components/home/logoutSession.jsx
@@ -27,7 +27,7 @@ export default function LogoutSession() {
 				</a>
 				<Link
 					className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-md text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 "
-					href="/teacher">
+					href="/income">
 					會員中心
 				</Link>
 				<Link
