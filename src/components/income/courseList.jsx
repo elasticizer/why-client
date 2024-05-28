ba84907fc20a20a75ab2d3e28757078864813637
Poyuan <poyuan510214@gmail.com>
2024-05-28 15:18:33 +0800

    新增下拉選單關鍵字搜尋

--- src/components/income/courseList.jsx
+++ src/components/income/courseList.jsx
@@ -1,28 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CourseListTbody from './courseListTbody';
-import SearchBox from './searchBox';
-import { useState, useEffect } from 'react';
+import SelectBox from './selectBox';
 
 export default function CourseList({ data }) {
+	const [desc, setDesc] = useState(true);
+	const [filteredData, setFilteredData] = useState(data);
+	const [searchKeyword, setSearchKeyword] = useState('');
+
+	useEffect(() => {
+		setFilteredData(data);
+	}, [data]);
+
+	const handleSearchChange = event => {
+		setSearchKeyword(event.target.value);
+		console.log(searchKeyword);
+	};
+
+	const handleSearchClick = () => {
+		const keyword = searchKeyword.toLowerCase();
+		const newFilteredData = data.filter(item => {
+			return (
+				item.Name.toLowerCase().includes(keyword) ||
+				item.WhenLaunched.includes(keyword)
+			);
+		});
+		setFilteredData(newFilteredData);
+	};
+
+	const handleClearSearch = () => {
+		setSearchKeyword('');
+		setFilteredData(data);
+	};
+
 	return (
 		<>
-			{/* <SearchBox /> */}
+			<SelectBox
+				searchKeyword={searchKeyword}
+				handleSearchChange={handleSearchChange}
+				handleSearchClick={handleSearchClick}
+				handleClearSearch={handleClearSearch}
+				data={data}
+			/>
+			{/* searchBar */}
+			{/* searchBar */}
 			{/* table */}
 			{/* thead */}
 			<div className="flex flex-row justify-between items-center px-4 py-2 border rounded-t-lg font-semibold bg-yellow-50">
 				<div className="w-1/2">課程名稱</div>
-				<div className="w-1/3 text-center">開課日期</div>
-				<div className="w-1/3 text-center">總收益</div>
+				<div className="w-1/3 text-center">開課時間</div>
+				<div
+					className="w-1/3 text-center cursor-pointer"
+					onClick={() => {
+						const sortedData = [...filteredData].sort((a, b) =>
+							desc
+								? new Date(a.Income) - new Date(b.Income)
+								: new Date(b.Income) - new Date(a.Income)
+						);
+						setFilteredData(sortedData);
+						setDesc(!desc);
+					}}>
+					課程收益 {desc ? '▼' : '▲'}
+				</div>
 			</div>
 			{/* end thead */}
-			{data.map((course, i) => {
-				return (
-					<CourseListTbody
-						key={i}
-						data={course}
-					/>
-				);
-			})}
+			{filteredData.map((course, i) => (
+				<CourseListTbody
+					key={i}
+					data={course}
+				/>
+			))}
 		</>
 	);
 }
--- src/components/income/salesData.jsx
+++ src/components/income/salesData.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SalesDataTbody from './salesDataTbody';
+import SelectBox from './selectBox';
 
 export default function SalesData() {
 	const [data, setData] = useState([]);
+	const [desc, setDesc] = useState(true);
+	const [searchKeyword, setSearchKeyword] = useState('');
+	const [filteredData, setFilteredData] = useState([]);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -13,33 +17,75 @@ export default function SalesData() {
 			} catch (error) {
 				setData([]);
 			}
-			console.log(data);
 		};
 		fetchData();
 	}, []);
 
+	useEffect(() => {
+		setFilteredData(data);
+	}, [data]);
+
+	const handleSearchChange = event => {
+		setSearchKeyword(event.target.value);
+		console.log(searchKeyword);
+	};
+
+	const handleSearchClick = () => {
+		const keyword = searchKeyword.toLowerCase();
+		const newFilteredData = data.filter(item => {
+			return (
+				item.Name.toLowerCase().includes(keyword) ||
+				item.WhenPaid.includes(keyword)
+			);
+		});
+		setFilteredData(newFilteredData);
+	};
+
+	const handleClearSearch = () => {
+		setSearchKeyword('');
+		setFilteredData(data);
+	};
+
 	return (
 		<>
+			<SelectBox
+				searchKeyword={searchKeyword}
+				handleSearchChange={handleSearchChange}
+				handleSearchClick={handleSearchClick}
+				handleClearSearch={handleClearSearch}
+				data={data}
+			/>
 			{/* table */}
 			{/* thead */}
 			<div className="flex flex-row justify-between items-center px-4 py-2 border rounded-t-lg font-semibold bg-yellow-50">
 				<div className="w-1/2">課程名稱</div>
-				<div className="w-1/3 text-center">售出日期</div>
-				<div className="w-1/3 text-center">收益金額</div>
+				<div
+					className="w-1/3 text-center cursor-pointer"
+					onClick={() => (
+						setFilteredData(
+							filteredData.sort((a, b) =>
+								desc
+									? new Date(b.WhenPaid) - new Date(a.WhenPaid)
+									: new Date(a.WhenPaid) - new Date(b.WhenPaid)
+							)
+						),
+						setDesc(!desc)
+					)}>
+					售出時間 {desc ? '▼' : '▲'}
+				</div>
+				<div className="w-1/3 text-center">課程價格</div>
 			</div>
 			{/* end thead */}
 			{/* tbody */}
-			{data.map((data, i) => {
-				return (
-					<SalesDataTbody
-						key={i}
-						name={data.Name}
-						identifier={data.Identifier}
-						date={data.WhenPaid}
-						price={data.Price}
-					/>
-				);
-			})}
+			{filteredData.map((data, i) => (
+				<SalesDataTbody
+					key={i}
+					name={data.Name}
+					identifier={data.Identifier}
+					date={data.WhenPaid}
+					price={data.Price}
+				/>
+			))}
 			{/* end tbody */}
 		</>
 	);
