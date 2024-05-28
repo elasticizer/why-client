d283aec620300056c265b90b4f57c2dd55e98a12
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-28 12:04:28 +0800

    結帳完成頁新增推薦課程元件

---
+++ src/components/checkout/recommended.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import CourseCard from './courseCard';
+
+export default function Recommended() {
+	const [courses, setCourses] = useState([]);
+
+	useEffect(() => {
+		getCourses();
+	}, []);
+
+	const getCourses = async () => {
+		const url = '/api/course/recommended';
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			setCourses(data);
+			console.log(data);
+		} catch (e) {
+			console.error(e);
+			setCourses([]);
+		}
+	};
+
+	return (
+		<>
+			{courses.map((course, i) => (
+				<CourseCard
+					key={course.i}
+					name={course.Name}
+          filename={course.Filename}
+				/>
+			))}
+		</>
+	);
+}
---
+++ src/pages/api/course/recommended.js
@@ -0,0 +1,15 @@
+import { onError, onNoMatch } from '@/handlers/router';
+import connection from '@/handlers/sqlite3';
+import { StatusCodes } from 'http-status-codes';
+import { createRouter } from 'next-connect';
+
+const router = createRouter();
+
+router.get(async (req, res) => {
+	const [results] = await connection.execute(
+		'SELECT Course.SN, Course.Identifier, Course.Name, Course.Price, Domain.Name AS DomainName, File.Filename FROM Course JOIN	Domain ON Course.DomainSN = Domain.SN JOIN File ON Course.ThumbnailSN = File.SN ORDER BY RANDOM() LIMIT 3'
+	);
+	res.status(StatusCodes.OK).json(results);
+});
+
+export default router.handler({ onError, onNoMatch });
