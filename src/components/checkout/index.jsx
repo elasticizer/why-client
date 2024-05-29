82e6354fc12737543ca73e53fcbef2b348e87d05
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-29 14:36:40 +0800

    訂單內容串接

--- src/components/checkout/index.jsx
+++ src/components/checkout/index.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import PayMethod from './payMethod';
 import Summary from './summary';
 import SummaryOrder from './summaryOrder';
 import PrevPageBtn from '../cart/prevPageBtn';
-import Link from 'next/link';
+import CouponSelect from './couponSelect';
 
 export default function Checkout() {
+	const [selectedCoupon, setSelectedCoupon] = useState(null);
+
 	return (
-		<>
-			<div className="container mt-8 max-w-[85rem] sm:px-6 lg:px-8 mx-auto my-8">
-				<PrevPageBtn
-					href="/cart"
-					prev="購物車"
-				/>
-				{/* 結帳畫面 */}
-				<div className="grid lg:grid-cols-4 gap-y-8 lg:gap-y-0 lg:gap-x-6">
-					<div className="lg:col-span-3">
-						<Summary />
-						<PayMethod />
-					</div>
-					<SummaryOrder />
+		<div className="container mt-8 max-w-[85rem] sm:px-6 lg:px-8 mx-auto my-8">
+			<PrevPageBtn
+				href="/cart"
+				prev="購物車"
+			/>
+			<div className="grid lg:grid-cols-4 gap-y-8 lg:gap-y-0 lg:gap-x-6">
+				<div className="lg:col-span-3">
+					<Summary />
+					<PayMethod />
+					<CouponSelect
+						setSelectedCoupon={setSelectedCoupon}
+						selectedCoupon={selectedCoupon}
+					/>
 				</div>
+				<SummaryOrder selectedCoupon={selectedCoupon} />
 			</div>
-		</>
+		</div>
 	);
 }
