817db9a9ce631422f07dc7a4dbb9cede8446f20d
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-30 17:02:46 +0800

    修改環境變數

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,12 +1,15 @@
 import CartLayout from '@/layouts/cart';
 import Cart from '@/components/cart';
+import 'animate.css';
 
 export default function CartPage() {
 	return (
 		<>
 			<CartLayout>
 				<title>購物車</title>
-				<Cart />
+				<div className="animate__animated animate__fadeInLeft">
+					<Cart />
+				</div>
 			</CartLayout>
 		</>
 	);
