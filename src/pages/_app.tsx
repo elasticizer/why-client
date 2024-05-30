0e67aff288870b5e0a45d1483cf2bdeba84f8fbd
ameandneo <a430193333@gmail.com>
2024-05-30 15:12:31 +0800

    移除loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import '@/styles/globals.css';
 import { AuthProvider } from '@/hooks/use-auth';
-import { LoaderProvider } from '@/hooks/use-loader';
 import ArtSideBar from '@/components/art/ArtSideBar';
 import { Toaster } from 'react-hot-toast';
 import type { AppProps } from 'next/app';
 import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Home from '@/layouts/home';
 export default function App({ Component, pageProps }: AppProps) {
 	const path = usePathname();	
 	const router = useRouter();
@@ -20,14 +18,14 @@ export default function App({ Component, pageProps }: AppProps) {
 		[path]
 	);
 	return (
-		<LoaderProvider>
+		
 			<AuthProvider>
 				{isArt && <ArtSideBar></ArtSideBar>}
-				<Home>
+			
 					<Component {...pageProps} />
-				</Home>
+				
 				<Toaster />
 			</AuthProvider>
-		</LoaderProvider>
+		
 	);
 }
