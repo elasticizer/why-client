5e8eb726ee060faed5d6a0fbbd0561609c20e682
jackall <dc951236@gmail.com>
2024-05-30 11:40:55 +0800

    修改部落格的Link裡面不能再包Link、banner帶真的人名

--- src/components/member/articleBookmarking/contentCard.jsx
+++ src/components/member/articleBookmarking/contentCard.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { BsFillTrash3Fill } from "react-icons/bs";
 import Swal from 'sweetalert2';
 import 'animate.css';
+import { stripHtml } from "string-strip-html";
 
 
 export default function ContentCard({
@@ -15,14 +16,10 @@ export default function ContentCard({
 	setDataSuccess }) {
 
 
-	// html轉純文字。
-	const stripHtmlTags = (html) => {
-		const doc = new DOMParser().parseFromString(html, 'text/html');
-		return doc.body.textContent || "";
-	};
+	const deletedData = async (e) => {
 
-	const deletedData = async () => {
 		try {
+
 			const result = await Swal.fire({
 				title: "確定要刪除該篇收藏嗎?",
 				text: "或許他還對妳有幫助!",
@@ -60,23 +57,22 @@ export default function ContentCard({
 					<time dateTime="2020-03-16" className="text-gray-500">
 						{WhenCreated}
 					</time>
-					<a
-						href="#"
+					<div
 						className="relative z-5 rounded-full bg-orange-100 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
 					>
 						Marketing
-					</a>
+					</div>
 				</div>
 				<div className="group relative w-full">
 					<h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-						<a href="#">
+						<div>
 							<span className="inset-0 line-clamp-1">{Title}</span>
 
-						</a>
+						</div>
 					</h3>
-					<p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 text-justify">
-						{stripHtmlTags(Content)}
-					</p>
+					<div className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 text-justify">
+						{Content && stripHtml(Content).result}
+					</div>
 				</div>
 				<div className="relative mt-8 flex w-full items-end gap-x-4 justify-between">
 					<div className="relative flex items-center gap-x-4">
@@ -86,15 +82,18 @@ export default function ContentCard({
 							className="h-10 w-10 rounded-full bg-gray-50"
 						/>
 						<div className="text-sm leading-6">
-							<p className="font-semibold text-gray-900">
-								<a href="#">
+							<div className="font-semibold text-gray-900">
+								<div>
 									<span className="absolute inset-0" />
 									{Author}
-								</a>
-							</p>
+								</div>
+							</div>
 						</div>
 					</div>
-					<button className="mb-1" onClick={deletedData}><BsFillTrash3Fill color='#7B7B7B' size="20px" /></button>
+					<button className="mb-1" onClick={(e) => {
+						deletedData(e);
+						e.preventDefault();
+					}}><BsFillTrash3Fill color='#7B7B7B' size="20px" /></button>
 				</div>
 
 			</article>
--- src/components/member/articleManagement/contentCard.jsx
+++ src/components/member/articleManagement/contentCard.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
 import { BsFillTrash3Fill } from "react-icons/bs";
 import Swal from 'sweetalert2';
 import 'animate.css';
+import { stripHtml } from "string-strip-html";
 
 
 export default function ContentCard({
@@ -16,12 +18,7 @@ export default function ContentCard({
 	setDataSuccess,
 	picUrl }) {
 
-
-		// html轉純文字。
-	const stripHtmlTags = (html) => {
-		const doc = new DOMParser().parseFromString(html, 'text/html');
-		return doc.body.textContent || "";
-	};
+	const router = useRouter();
 
 	const deletedData = async () => {
 		try {
@@ -63,22 +60,21 @@ export default function ContentCard({
 					<time dateTime="2020-03-16" className="text-gray-500">
 						{WhenCreated}
 					</time>
-					<a
-						href="#"
-						className="relative z-5 rounded-full bg-orange-100 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+					<div
+						className="relative z-5 rounded-full bg-orange-100 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100" onClick={() => { router.push('/'); }}
 					>
 						Marketing
-					</a>
+					</div>
 				</div>
 				<div className="group relative w-full">
 					<h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-						<a href="#">
+						<div href="#">
 							<span className="inset-0 line-clamp-1">{Title}</span>
 
-						</a>
+						</div>
 					</h3>
 					<p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600 text-justify">
-						{stripHtmlTags(Content)}
+						{Content && stripHtml(Content).result}
 					</p>
 				</div>
 				<div className="relative mt-8 flex w-full items-end gap-x-4 justify-between">
@@ -89,15 +85,18 @@ export default function ContentCard({
 							className="h-10 w-10 rounded-full bg-gray-50"
 						/>
 						<div className="text-sm leading-6">
-							<p className="font-semibold text-gray-900">
-								<a href="#">
+							<div className="font-semibold text-gray-900">
+								<div>
 									<span className="absolute inset-0" />
 									{Author}
-								</a>
-							</p>
+								</div>
+							</div>
 						</div>
 					</div>
-					<button className="mb-1" onClick={deletedData}><BsFillTrash3Fill color='#7B7B7B' size="20px" /></button>
+					<button className="mb-1" onClick={(e) => {
+						e.preventDefault();
+						deletedData();
+					}}><BsFillTrash3Fill color='#7B7B7B' size="20px" /></button>
 				</div>
 
 			</article>
