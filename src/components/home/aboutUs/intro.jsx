e0b969692340d811e4f7945a413e26fbc8a9924e
Poyuan <poyuan510214@gmail.com>
2024-05-30 09:33:48 +0800

    版面微調

--- src/components/home/aboutUs/intro.jsx
+++ src/components/home/aboutUs/intro.jsx
@@ -14,12 +14,14 @@ export default function Intro() {
 						/>
 					</div>
 					<div className="flex flex-col">
-						<h1 className="mb-5 text-center text-xl text-gray-600 sm:text-4xl">
+						<h1 className="mb-5 text-center text-2xl text-gray-600 sm:text-4xl">
 							關於我們
 						</h1>
 						<div className="flex justify-center">
 							<p className="text-center text-lg max-w-[50rem]">
-								讓每個有意追求理想生活的人，都有機會透過持續探索、分享學習，創造更多選擇與可能性，學有所成，成為理想的自己。讓我們以學習賦能，自定義自我價值。
+								讓每個有意追求理想生活的人，都有機會透過持續探索、分享學習，創造更多選擇與可能性。
+								<br />
+								學有所成，成為理想的自己。讓我們以學習賦能，自定義自我價值。
 							</p>
 						</div>
 					</div>
