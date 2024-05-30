632873cc53be24aadc5ee3782d3cfeaefb7b1bf2
Poyuan <poyuan510214@gmail.com>
2024-05-30 09:34:15 +0800

    修改api篩選內容

--- src/pages/api/course/index.js
+++ src/pages/api/course/index.js
@@ -10,7 +10,7 @@ router.get(async (req, res) => {
 	const limit = req.query.limit ?? 10;
 
 	const [results] = await connection.execute(
-		'SELECT Course.SN, Course.Name, Course.Intro,Course.Price,File.FileName, Domain.Name AS DomainName FROM Course JOIN File ON Course.ThumbnailSN = File.SN JOIN Domain ON Course.DomainSN = Domain.SN ORDER BY Course.SN DESC'
+		'SELECT Course.SN, Course.Name, Course.Intro,Course.WhenLaunched,Course.Price,File.FileName, Domain.Name AS DomainName FROM Course JOIN File ON Course.ThumbnailSN = File.SN JOIN Domain ON Course.DomainSN = Domain.SN ORDER BY WhenLaunched DESC'
 	);
 
 	return res.status(200).json(results);
