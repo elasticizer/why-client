836215095eb2473a22c8f097121198f6620fb51e
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-31 12:25:15 +0800

    更改購物車連結

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -10,7 +10,7 @@ export default function Cart() {
 	return (
 		<div className="container mt-8 max-w-[85rem] sm:px-6 lg:px-8 mx-auto my-8">
 			<PrevPageBtn
-				href="/test"
+				href="/course"
 				prev="課程頁"
 			/>
 			<div className="grid lg:grid-cols-4 gap-y-8 lg:gap-y-0 lg:gap-x-6">
