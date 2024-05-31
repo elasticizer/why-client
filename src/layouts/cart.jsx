39dad02a394f4126255fe266fe88dd78a43271fc
Peiru <155552454+linruu827@users.noreply.github.com>
2024-05-31 12:15:56 +0800

    修正layouts

--- src/layouts/cart.jsx
+++ src/layouts/cart.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Navbar from '@/components/home/navbar.jsx';
-import Footer from '@/components/home/footer.jsx';
 import { CartProvider } from '@/contexts/cart';
 import { CouponProvider } from '../contexts/coupon';
 
@@ -8,9 +6,7 @@ export default function CartLayout({ children }) {
 	return (
 		<CartProvider>
 			<CouponProvider>
-				<Navbar />
 				{children}
-				<Footer />
 			</CouponProvider>
 		</CartProvider>
 	);
