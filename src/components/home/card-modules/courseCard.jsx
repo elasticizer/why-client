6553fe7ca6b1dfdc89bb5def5e5c5fc3c3450668
Poyuan <poyuan510214@gmail.com>
2024-05-31 09:16:07 +0800

    最新課程圖片&文字格式調整

--- src/components/home/card-modules/courseCard.jsx
+++ src/components/home/card-modules/courseCard.jsx
@@ -4,11 +4,15 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import Tags from '@/components/home/card-modules/tags';
+import { stripHtml } from 'string-strip-html';
 
 export default function CourseCard({ data }) {
 	useEffect(() => {
 		AOS.init();
 	}, []);
+
+	const imgFile = data.Filename;
+
 	return (
 		<>
 			<a
@@ -17,7 +21,11 @@ export default function CourseCard({ data }) {
 				<div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
 					<img
 						className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
-						src={data.Filename}
+						src={
+							imgFile && imgFile.startsWith('h')
+								? imgFile
+								: `/learner/LessonVideo/${imgFile}`
+						}
 						alt="Image Description"
 					/>
 				</div>
@@ -28,7 +36,9 @@ export default function CourseCard({ data }) {
 					<h3 className="text-md font-semibold text-gray-800 group-hover:text-gray-600 line-clamp-1">
 						{data.Name}
 					</h3>
-					<div className="mt-1 text-gray-800 line-clamp-2">{data.Intro}</div>
+					<div className="mt-1 text-gray-800 line-clamp-2">
+						{data.Intro && stripHtml(data.Intro).result}
+					</div>
 					<div className="flex mt-2 items-center gap-2">
 						<div className="text-sky-500 text-xl font-semibold">NT:</div>
 						<div className="text-orange-500 text-2xl font-semibold">
