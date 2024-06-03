b84b6acb84afc20f7ea09d0413042ec036175490
Peiru <155552454+linruu827@users.noreply.github.com>
2024-06-03 15:03:25 +0800

    更新連結、判斷課程圖片連結

--- src/components/checkout/courseCard.jsx
+++ src/components/checkout/courseCard.jsx
@@ -6,7 +6,11 @@ export default function CourseCard({ name, filename }) {
 			<div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
 				<img
 					className="size-full absolute top-0 start-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-					src={filename}
+					src={
+							filename && filename.startsWith('h')
+								? filename
+								: `/learner/LessonVideo/${filename}`
+						}
 				/>
 			</div>
 			<div className="p-4 md:p-5">
--- src/components/order/orderItemNone.jsx
+++ src/components/order/orderItemNone.jsx
@@ -1,16 +1,18 @@
-import { PiListPlusBold } from "react-icons/pi";
+import { PiListPlusBold } from 'react-icons/pi';
+import Link from 'next/link';
 export default function OrderItemNone() {
 	return (
 		<>
 			<div className="flex flex-col items-center justify-center space-y-4 p-36">
-			<p className="text-9xl "><PiListPlusBold /></p>
+				<p className="text-9xl ">
+					<PiListPlusBold />
+				</p>
 				<p>尚未有訂單</p>
-				<a
-					type="button"
-					href="#"
-					className="py-2 px-10 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-400 text-white hover:bg-orange-500 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
-					前往首頁
-				</a>
+				<Link href={'/'}>
+					<p className="py-2 px-10 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-400 text-white hover:bg-orange-500 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
+						前往首頁
+					</p>
+				</Link>
 			</div>
 		</>
 	);
